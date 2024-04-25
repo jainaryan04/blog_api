2c99f86304535a76c1bdc9c97eed9fa316851528
Aryan Jain <aryanrameshjain@gmail.com>
2024-04-25 19:28:14 +0530

    Add likes and delete functionality

--- index.js
+++ index.js
@@ -64,15 +64,35 @@ const db = new pg.Client({
         [req.body.title, email]);
     const resul = await db.query("UPDATE users SET content = ARRAY_APPEND(content, $1) WHERE email=$2;",
           [req.body.content, email]);    
+    const resu = await db.query("UPDATE users SET likes = ARRAY_APPEND(likes, $1) WHERE email=$2;",
+          [0, email]);
       res.redirect("/blog")
   })
-var title,content
+var title,content,likes,length
   app.get("/blog",async(req,res)=>{
     title=await db.query("SELECT title FROM users WHERE email=$1",[email]);
     content=await db.query("SELECT content FROM users WHERE email=$1",[email]);
-    console.log(title.rows[0].title)
-    console.log(title.rows[0].title.length)
-    res.render("blog.ejs",{title:title.rows[0].title,email:email,length:title.rows[0].title.length,content:content.rows[0].content})
+    likes=await db.query("SELECT likes FROM users WHERE email=$1",[email]);
+    length=title.rows[0].title.length
+    likes=likes.rows[0].likes
+    title=title.rows[0].title
+    content=content.rows[0].content
+    res.render("blog.ejs",{likes:likes,title:title,email:email,length:length,content:content})
+  })
+
+  app.get("/like/:id",async(req,res)=>{
+    likes[req.params.id]=likes[req.params.id]+1;
+    const r=await db.query("UPDATE users SET likes= $1 WHERE email=$2",[likes,email])
+    res.redirect("/blog")
+  })
+var i;
+  app.get("/delete/:id",async(req,res)=>{
+    i=req.params.id
+    title.splice(i, 1);
+    content.splice(i, 1);
+    likes.splice(i, 1);
+    const s=await db.query("UPDATE users SET likes= $1,content=$2,title=$3 WHERE email=$4",[likes,content,title,email])
+    res.redirect("/blog")
   })
 
 
